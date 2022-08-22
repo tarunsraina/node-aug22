9862833125989252f6215a733ab673a3fc1eaf56
Tarun teja <tarunsraina483@gmail.com>
2022-08-22 12:44:53 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,111 @@
+
+
+function getTodos() {
+    //console.log('GET Request');
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=3')
+    .then(res=>showOutput(res))
+    .catch(err=>console.error(err))
+  }
+  
+  // POST REQUEST
+  function addTodo() {
+    axios.post('https://jsonplaceholder.typicode.com/todos',{
+      title : 'Tarun todo',
+      completed : false
+    })
+    .then(res=>showOutput(res))
+    .catch(err=>console.error(err))
+  }
+  
+  // PUT/PATCH REQUEST
+  function updateTodo() {
+    axios.put('https://jsonplaceholder.typicode.com/todos/1',{
+      title : 'Tarun updated todo',
+      completed : true
+    })
+    .then(res=>showOutput(res))
+    .catch(err=>console.error(err))
+  }
+  
+  // DELETE REQUEST
+  function removeTodo() {
+    axios.put('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res=>showOutput(res))
+    .catch(err=>console.error(err))
+  }
+  
+  // SIMULTANEOUS DATA
+  function getData() {
+    axios.all([
+      axios.get('https://jsonplaceholder.typicode.com/todos'),
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+    ])
+    .then(axios.spread((todos,posts)=>showOutput(posts)))
+    .catch(err=>console.error(err))
+    
+  }
+  
+  // CUSTOM HEADERS
+  function customHeaders() {
+    console.log('Custom Headers');
+  }
+  
+  // TRANSFORMING REQUESTS & RESPONSES
+  function transformResponse() {
+    console.log('Transform Response');
+  }
+  
+  // ERROR HANDLING
+  function errorHandling() {
+    console.log('Error Handling');
+  }
+  
+  // CANCEL TOKEN
+  function cancelToken() {
+    console.log('Cancel Token');
+  }
+  
+  // INTERCEPTING REQUESTS & RESPONSES
+  
+  // AXIOS INSTANCES
+  
+  // Show output in browser
+  function showOutput(res) {
+    document.getElementById('res').innerHTML = `
+    <div class="card card-body mb-4">
+      <h5>Status: ${res.status}</h5>
+    </div>
+    <div class="card mt-3">
+      <div class="card-header">
+        Headers
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+      </div>
+    </div>
+    <div class="card mt-3">
+      <div class="card-header">
+        Data
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.data, null, 2)}</pre>
+      </div>
+    </div>
+    <div class="card mt-3">
+      <div class="card-header">
+        Config
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.config, null, 2)}</pre>
+      </div>
+    </div>
+  `;
+  }
+  
+  // Event listeners
+  document.getElementById('get').addEventListener('click', getTodos);
+  document.getElementById('post').addEventListener('click', addTodo);
+  document.getElementById('update').addEventListener('click', updateTodo);
+  document.getElementById('delete').addEventListener('click', removeTodo);
+  document.getElementById('sim').addEventListener('click', getData);
+  
